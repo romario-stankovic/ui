383ed6a04fb879a8b9838272391fbb00e0f5694e
romario-stankovic <romario.01.orljevo@gmail.com>
2024-12-30 23:59:08 +0100

    implement a couple of different button variants

--- library/src/components/button/button.lite.tsx
+++ library/src/components/button/button.lite.tsx
@@ -1,6 +1,14 @@
 import { Slot } from "@builder.io/mitosis";
 import "src/components/button/button.scss";
 
-export default function Button() {
-    return <button><Slot /></button>;
+interface ButtonProps {
+    variant?: "flat" | "soft" | "outlined" | "colored-outline";
+}
+
+export default function Button(props: ButtonProps) {
+    return (
+        <button class={`button ${props.variant ?? "flat"}`}>
+            <span><Slot /></span>
+        </button>
+    );
 }
--- tests/stories/button.stories.tsx
+++ tests/stories/button.stories.tsx
@@ -6,9 +6,48 @@ type Story = StoryObj<typeof Button>;
 export default {
     title: "Components/Button",
     component: Button,
-    tags: ['autodocs']
+    tags: ['autodocs'],
+    argTypes: {
+        default: {
+            control: "text",
+            description: "Default text of the button"
+        },
+        variant: {
+            control: "select",
+            options: ["flat", "soft", "outlined", "colored-outline"],
+            description: "Appearance of the button"
+        },
+        color: {
+            control: "color"
+        }
+    },
+    args: {
+        default: "Button",
+        variant: "flat"
+    },
+    render: (args) => <Button {...args}>{args.default}</Button>
 } satisfies Meta;
 
-export const Default = {
+export const Flat: Story = {
+    args: {
+        variant: "flat"
+    }
+};
 
+export const Soft: Story = {
+    args: {
+        variant: "soft"
+    }
+};
+
+export const Outlined: Story = {
+    args: {
+        variant: "outlined"
+    }
+};
+
+export const ColoredOutline: Story = {
+    args: {
+        variant: "colored-outline"
+    }
 };
