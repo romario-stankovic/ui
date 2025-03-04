030cc3a793e6e6635389dd95ec165bcb870fb6c4
romario-stankovic <romario.01.orljevo@gmail.com>
2025-03-04 22:55:40 +0100

    add sanitizer to markdown component

--- src/components/markdown/markdown.lite.tsx
+++ src/components/markdown/markdown.lite.tsx
@@ -1,18 +1,26 @@
 import { useDefaultProps } from "@builder.io/mitosis";
-import { marked } from "marked";
+import { marked, MarkedOptions } from "marked";
 import style from "./markdown.scss";
+import insane, { SanitizeOptions } from "insane";
 
 interface MarkdownProps {
     markdown?: string;
+    markedOptions?: Omit<MarkedOptions, "async">;
+    sanitizeOptions?: SanitizeOptions;
 }
 
 export default function Markdown(props: MarkdownProps) {
     useDefaultProps<typeof props>({
-        markdown: ""
+        markdown: "",
+        markedOptions: undefined,
+        sanitizeOptions: undefined
     });
 
     function getMarkdown() {
-        return marked.parse(props.markdown ?? "", { async: false });
+        return insane(
+            marked.parse(props.markdown ?? "", { ...props.markedOptions, async: false }),
+            props.sanitizeOptions
+        );
     }
 
     return <div class="markdown" innerHTML={getMarkdown()} />;
